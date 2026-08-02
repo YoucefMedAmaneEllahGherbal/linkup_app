000818bfd2d4da4ce8836bfca548dfcec9c31c25
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-02 18:45:27 +0100

    Add modal progress HUD for loading indicators in Login and Registration screens

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
